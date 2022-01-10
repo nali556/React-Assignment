d22df8b8ffb81da6a92c07e8c18ff582c3ae978c
Noman Ali <noman710@protonmail.com>
2022-01-09 23:30:03 -0500

    Lifting state up completed

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import './index.css';
 class Square extends React.Component {
     render() {
       return (
-        <button className="square" onClick={function() { console.log("click")}}>
+        <button className="square"
+        onClick={() => this.props.onClick({value: 'X'})}
+        >
           {this.props.value}
         </button>
       );
@@ -13,8 +15,25 @@ class Square extends React.Component {
   }
   
   class Board extends React.Component {
+      constructor(props){
+          super(props)
+          this.state = {
+              squares: Array(9).fill(null)
+          }
+      }
+
+      handleClick(i){
+          const squares = this.state.squares.slice()
+          squares[i] = 'X'
+          this.setState({squares: squares})
+      }
+
+
     renderSquare(i) {
-      return <Square value ={i} />
+      return <Square
+       value ={this.state.squares[i]}
+       onClick={() => this.handleClick(i)}
+        />
     }
   
     render() {
